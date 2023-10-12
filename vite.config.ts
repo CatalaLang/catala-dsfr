e1655122b91020262802da1b1cfba3be97ebfd47
Emile Rolley <emile.rolley@tuta.io>
2023-10-12 11:10:26 +0200

    build: upgrade the config and packages

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,14 @@
 import { defineConfig } from "vite";
 import tailwindcss from "tailwindcss";
 import react from "@vitejs/plugin-react";
-import createReScriptPlugin from "@jihchi/vite-plugin-rescript";
+import rescript from "@jihchi/vite-plugin-rescript";
 import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), createReScriptPlugin(), viteCommonjs()],
+  plugins: [react(), rescript(), viteCommonjs()],
   server: {
     watch: {
-      // include: ["./src/**/*.{js, tsx}", "**/assets/**/*"],
       ignored: ["**/dsfr/**/*", "**/node_modules/**/*"],
     },
   },
