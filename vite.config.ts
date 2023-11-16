bc04a2f5120862af5e2a0f77de17e28c15cc1a9d
Emile Rolley <emile.rolley@tuta.io>
2023-11-16 13:02:05 +0100

    build: add a custom plugin to accept path containing dots (like v0.9.0)

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,29 @@ import react from "@vitejs/plugin-react";
 import rescript from "@jihchi/vite-plugin-rescript";
 import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 
+// Needed to allow ending paths with a version number (e.g. /v1.0.0), if not
+// the server will try to find a file with that name and fail.
+const allowPathWithEndingVersionNumbers = () => ({
+  name: "allow-path-with-ending-version-numbers",
+  configureServer: (server) => {
+    server.middlewares.use((req, _, next) => {
+      const endPath = req.url.split("?", 2)[0].split("/").slice(-1)[0];
+      if (endPath.match(/v[0-9]\.[0-9]\.[0-9]/)) {
+        req.url = "/";
+      }
+      next();
+    });
+  },
+});
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), rescript(), viteCommonjs()],
+  plugins: [
+    react(),
+    rescript(),
+    viteCommonjs(),
+    allowPathWithEndingVersionNumbers(),
+  ],
   server: {
     watch: {
       ignored: ["**/dsfr/**/*", "!**/node_modules/@catala-lang/**/*"],
